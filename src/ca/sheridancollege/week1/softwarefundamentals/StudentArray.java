44260adcbd114801e878ff8af259bece79abc587
elizabethdancy <39062081+elizabethdancy@users.noreply.github.com>
2019-01-23 12:53:03 -0500

    Update StudentArray.java
    Added month to the copyright

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -7,7 +7,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
  * A class to create an array of students and print their names
- * @author dancye, 2019
+ * @author dancye, January 2019
  */
 public class StudentArray 
 {
